262b1dc4efb120918596c17bbb55193a3ab47b71
Christian John jiménez Calvo <christianjjc@mail.com>
2024-09-27 08:58:57 -0500

    App Runinig on Port 3001

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.port);
+  logger.log(`App Running on port ${envs.port}`);
+}
+bootstrap();
