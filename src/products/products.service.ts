ec489c8e0ceb4a598d6eeba35bbd4153063c3eca
Christian John jiménez Calvo <christianjjc@mail.com>
2024-09-28 17:01:45 -0500

    FIN SECCION 07

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -86,4 +86,22 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
     return { product: productToDelete };
   }
+
+  async validateProducts(ids: number[]) {
+    ids = Array.from(new Set(ids));
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+    if (products.length !== ids.length) {
+      throw new RpcException({
+        message: `Some products were Not Found`,
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+    return products;
+  }
 }
